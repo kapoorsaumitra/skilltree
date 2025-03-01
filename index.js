f56e5a7fb2b7a08ef06f7d0826eeb80c4812e6cf
Saumitra Kapoor <saumitrakapoor24@gmail.com>
2025-03-01 13:03:49 +0530

    Add basic Express server with user and course routes

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const jwt = require("jsonwebtoken")
+
+
+const app = express()
+
+app.post("/user/signup", function(req,res){
+    
+})
+app.post("/user/login", function(req,res){
+    
+})
+app.get("/courses", function(req,res){
+    
+})
+app.get("/user/purchases", function(req,res){
+    
+})
+app.post("/course/purchase", function(req,res){
+    
+})
+
+
+app.listen(3000)
