6ab4f003ec5f60723cc2a35f0c47d89749d908be
Saumitra Kapoor <saumitrakapoor24@gmail.com>
2025-03-01 19:56:49 +0530

    Add database models and dotenv configuration for user, admin, course, and purchase

---
+++ db.js
@@ -0,0 +1,45 @@
+require('dotenv').config()
+const mongoose = require("mongoose")
+mongoose.connect(process.env.MONGO_DB_URL)
+
+const Schema = mongoose.Schema
+const ObjectId = mongoose.Types.ObjectId
+
+const userSchema = new Schema({
+    email : {type:String, unique:true},
+    password : String,
+    firstName : String,
+    lastName : String
+})
+
+const adminSchema = new Schema({
+    email : {type: String, unique:true},
+    password : String,
+    firstName: String,
+    lastName: String
+})
+
+const courseSchema = new Schema({
+    title : String,
+    description : String,
+    price : Number,
+    imageUrl : String,
+    creatorId : ObjectId
+})
+
+const purchaseSchema = new Schema({
+    courseId : ObjectId,
+    userId : ObjectId
+})
+
+const userModel = mongoose.model("user",userSchema)
+const adminModel = mongoose.model("admin",adminSchema)
+const courseModel = mongoose.model("course",courseSchema)
+const purchaseModel = mongoose.model("purchase", purchaseSchema)
+
+module.exports = {
+    userModel: userModel,
+    adminModel: adminModel,
+    courseModel: courseModel,
+    purchaseModel: purchaseModel
+}
